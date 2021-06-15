a1ed3d9569e5728950b577e40e435f835aadbddb
jishnu19048 <jishnu19048@iiitd.ac.in>
2021-06-15 15:46:24 +0530

    padding added to experience tag

--- src/components/about.js
+++ src/components/about.js
@@ -26,7 +26,7 @@ export default class About extends Component {
           >
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg> */}
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 mr-10">
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 lg:mr-10">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Hello,</span>{' '}
