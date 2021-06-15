c889dad11af4c2738275e51a904f35f8a342ae15
jishnu19048 <jishnu19048@iiitd.ac.in>
2021-06-15 16:56:38 +0530

    minor changes#1

--- src/components/about.js
+++ src/components/about.js
@@ -53,7 +53,7 @@ export default class About extends Component {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1612809856000-898764bbd504?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+          src="https://images.unsplash.com/photo-1592142878435-6e2f5c5362d9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
           alt=""
         />
       </div>
