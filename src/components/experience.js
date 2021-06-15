62a980fd35dc01f0aff1aad7d5dcad41584c563b
jishnu19048 <jishnu19048@iiitd.ac.in>
2021-06-15 13:28:57 +0530

    logo

--- src/components/experience.js
+++ src/components/experience.js
@@ -22,7 +22,7 @@ export default class Experience extends Component{
                         </div>
                         <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
                         <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                            <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="http://midas.iiitd.edu.in/assets/themes/lab/images/logo/LOGO.png" />
+                            <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://pbs.twimg.com/profile_images/710727346148409344/88A6YRY2.jpg" />
                             <div className="flex-grow">
                             <h2 className="text-gray-900 title-font font-medium">MIDAS Labs, IIITD</h2>
                             <p className="text-gray-500">Software Developer</p>
